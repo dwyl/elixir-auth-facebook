6e1c627988255989e7b696b1bd97429d67a26ecf
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2015-11-27 20:12:38 +0000

    #8. moved endpoints into options.

--- example/facebook_server.js
+++ example/facebook_server.js
@@ -8,10 +8,29 @@ server.connection({
   port: Number(process.env.PORT)
 });
 
-server.register({ register: facebookAuth }, {
+server.register({
+  register: facebookAuth,
+  options: {
+    handler: function(request, reply, accessToken) {
+      reply('Your token: ' + accessToken);
+    },
+    redirectUri: '/facebookLogin',
+    tokenRequestPath: '/authFacebook'
+  }
 }, function (err) {
   if (err) console.log(err);
 });
+
+server.route({
+  path: '/facebook',
+  method: 'GET',
+  handler: function(request, reply) {
+    var btn = '<a href="' + '/authFacebook' +
+      '"><img src="http://i.stack.imgur.com/pZzc4.png"></a>';
+    reply(btn);
+  }
+})
+
 
 server.start(function() {
   console.log("Server listening on " + server.info.uri);
