346ce7930b7b0018bcca5472620f6f74f94ba94c
Huw <hdrdavies@gmail.com>
2015-11-27 12:14:01 +0000

    login button working

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+var env    = require('env2')('.env');
 var Hapi   = require('hapi');
+
 var server = new Hapi.Server();
+
 
 server.connection({
   host: '0.0.0.0',
@@ -11,9 +14,16 @@ server.route([
   { method: 'GET',
     path: '/',
     handler: function (request, reply) {
-      var url = 'http://google.com';
-      var btn = '<a href="' + url + '"><img src="http://i.stack.imgur.com/pZzc4.png"></a>'
-      reply(btn)
+      var redirectUri = 'http://0.0.0.0:8000/login';
+      var url = 'https://www.facebook.com/dialog/oauth?client_id=' + process.env.FACEBOOK_APP_ID + '&redirect_uri=' + redirectUri;
+      var btn = '<a href="' + url + '"><img src="http://i.stack.imgur.com/pZzc4.png"></a>';
+      reply(btn);
+    }
+  },
+  { method: 'GET', // yeah?
+    path: '/login',
+    handler: function (request, reply) {
+      reply("login successful!!");
     }
   }
 ]);
