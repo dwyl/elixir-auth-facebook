ed37387c28dc9da4cef16602aed6a32b24d0b479
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2015-11-28 10:11:04 +0000

    first attempt at mock testing

--- test/facebook.test.js
+++ test/facebook.test.js
@@ -1,16 +1,43 @@
 var test = require('tape');
+var nock = require('nock');
+var dir = __dirname.split('/')[__dirname.split('/').length - 1];
+var file = dir + __filename.replace(__dirname, '') + ' > ';
 
 var server = require('../example/facebook_server.js');
 
-test('our first test!', function(t) {
+test(file + 'our first test!', function(t) {
   var options = {
     method: 'GET',
     url: '/facebook'
   };
   server.inject(options, function(response) {
-    t.equal(response.statusCode, 200, 'woop')
+    t.equal(response.statusCode, 200, 'woop');
     setTimeout(function() {
       server.stop(t.end);
     }, 100);
   });
-})
+});
+
+var mockToken = {
+  "access_token":"d9d650834c5c844dce6757a10149ac11b838b0e1",
+  "token_type":"bearer",
+  "scope":"repo"
+};
+
+
+
+test(file + 'first nock test', function(t) {
+  var options = {
+    method: 'GET',
+    url: '/facebook'
+  };
+  var nock  = require('nock');
+  var scope = nock('https://graph.facebook.com')
+              .get('/v2.3/oauth/access_token?')
+              .reply(200, mockToken);
+      server.inject(options, function(response) {
+        t.equal(response.statusCode, 200, "Mock Test Working!");
+        server.stop(t.end);
+      });
+
+});
